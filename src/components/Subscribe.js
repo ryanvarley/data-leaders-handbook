a5210cde6f6726f0a1c9995c9d52937a811fa34d
Ryan Varley <ryanjvarley@gmail.com>
2025-02-02 12:20:58 +0000

    add subscribe in article

---
+++ src/components/Subscribe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function SubscribeNoLogo() {
+    return (
+        <div>
+            <iframe
+                src="https://blog.dataleadership.ai/embed"
+                width="480"
+                height="150"
+                style={{ border: '1px solid #EEE', background: 'white' }}
+                frameBorder="0"
+                scrolling="no"
+            />
+        </div>
+    );
+}
