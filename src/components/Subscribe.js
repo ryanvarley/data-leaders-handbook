a5db3ab19cfdb63c7ac01f9f7b69f55f76f6ca2d
Ryan Varley <ryanjvarley@gmail.com>
2025-02-03 20:42:38 +0000

    better subscribe

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-export default function SubscribeNoLogo() {
+export default function Subscribe({ withLogo }) {
+    const height = withLogo ? 320 : 150;
+
     return (
         <div>
             <iframe
                 src="https://blog.dataleadership.ai/embed"
                 width="480"
-                height="150"
+                height={height}
                 style={{ border: '1px solid #EEE', background: 'white' }}
                 frameBorder="0"
                 scrolling="no"
